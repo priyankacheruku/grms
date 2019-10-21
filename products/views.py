e65d3f5353feaf1b833cf9c646f3cd216157433d
priyankacheruku <priyankacheruku2016@gmail.com>
2019-10-21 23:55:35 +0530

    search field added with js and css

--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
+from django.http import HttpResponse
+from django.db import models
+
+from .models import *
 
 def index(request):
     #return HttpResponse('Hello, welcome to the index page.')
-    return render(request, 'home.html', {'products':'products will be viewed here'})
+    l=[]
+    items = item.objects.all()
+    for i in items:
+    	l.append(i.name)
+    return render(request, 'home.html', {'products':l})
