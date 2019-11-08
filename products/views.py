81a778f179b1357d61b7dc2051a2c824a8bfced0
priyankacheruku <priyankacheruku2016@gmail.com>
2019-11-08 22:49:44 +0530

    total feild added

--- products/views.py
+++ products/views.py
@@ -18,14 +18,14 @@ items = item.objects.all()
 #only item name are added
 for i in items:
    	l.append(i.name)
-
-
+total_amount=0
 def index(request):
 	bill.clear()
 	final.clear()
 	
-
-	return render(request, 'home.html', {'products':l})
+	global total_amount
+	total_amount=0
+	return render(request, 'home.html', {'products':l,'total': total_amount})
     
 def add(request):
 	entry=[]
@@ -33,10 +33,16 @@ def add(request):
 	bill.append(x)
 
 	#query to get price and id of items enterd
-	entry = list(item.objects.filter(name=x).values_list('name','price','id'))
+	entry = item.objects.filter(name=x).values_list('name','price','id')
+	print(entry)
+	try:
+		
+		global total_amount
+		total_amount=total_amount+entry[0][1]
 
-
-
+	except IndexError:
+		return HttpResponse("null or unavailable value entered is  not accepted")
+		
 	for e in entry:		
 		final.append("<tr>")
 		for i in e:
@@ -44,12 +50,15 @@ def add(request):
 			final.append(str(i))
 			final.append("</td>")
 		final.append("</tr>")
-	
+		
 
 	f=''.join(final)
 	context={
 		'products':l,
 		'bill_items':bill,
-		'bill':f
+		'bill':f,
+		'table_header':"<tr><th>Name</th><th>Price</th><th>Id</th></tr>",
+		'total': total_amount
 	}
+
 	return render(request, 'home.html', context)
